441cff94a75008e27e6b9cff1acf3df6123eb275
gichang.lee <ratsgo@naver.com>
2020-08-26 13:25:37 +0900

    [python] #10 acc 계산 로직 보완

--- ratsnlp/nlpbook/metrics.py
+++ ratsnlp/nlpbook/metrics.py
@@ -7,6 +7,8 @@ def accuracy(preds, labels, ignore_index=None):
         correct = torch.sum(preds == labels)
         total = torch.sum(torch.ones_like(labels))
         if ignore_index is not None:
-            correct -= torch.sum(preds == ignore_index)
+            # 모델이 맞춘 것 가운데 ignore index에 해당하는 것 제외
+            correct -= torch.sum(torch.logical_and(preds == ignore_index, preds == labels))
+            # accuracy의 분모 가운데 ignore index에 해당하는 것 제외
             total -= torch.sum(labels == ignore_index)
-    return correct / total
+    return correct.to(dtype=torch.float) / total.to(dtype=torch.float)
