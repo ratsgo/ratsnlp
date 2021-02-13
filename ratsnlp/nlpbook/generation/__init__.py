fb9090e920f7562844db6072671c6fa21ffef4c8
ratsgo <ratsgo@naver.com>
2021-02-13 23:12:51 +0900

    [Python] #6 토크나이저 위치 변경

--- ratsnlp/nlpbook/generation/__init__.py
+++ ratsnlp/nlpbook/generation/__init__.py
@@ -1,4 +1,3 @@
 from .corpus import GenerationDataset, NsmcCorpus
 from .task import GenerationTask
-from .tokenizer import KoGPT2Tokenizer
 from .arguments import GenerationTrainArguments, GenerationDeployArguments
---
+++ ratsnlp/nlpbook/tokenizers/__init__.py
@@ -0,0 +1,1 @@
+from .kogpt2 import KoGPT2Tokenizer
