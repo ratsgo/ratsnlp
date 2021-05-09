7dc7a5ada194c42f744565e0ad7ab7f40dd8217c
ratsgo <ratsgo@naver.com>
2021-05-09 19:57:06 +0900

    [Python] 문장 생성 튜토리얼 보완

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ratsnlp",
-    version="0.0.988",
+    version="0.0.989",
     license='MIT',
     author="ratsgo",
     author_email="ratsgo@naver.com",
