b7ef23c41f650209a09a559ede387c07dbd3d5fd
gichang.lee <ratsgo@naver.com>
2021-06-26 15:24:48 +0900

    [python] 의존성 패키지 보완

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def requirements():
 
 setuptools.setup(
     name="ratsnlp",
-    version="0.0.9992",
+    version="0.0.9998",
     license='MIT',
     author="ratsgo",
     author_email="ratsgo@naver.com",
